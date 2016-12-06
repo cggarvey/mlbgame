e10b5044fc7dc7874ba0babab62ce582120ef8ad
Cullen Garvey <cullen.garvey@gmail.com>
2016-12-06 03:04:11 -0600

    Broke out all read/write functions in to separate components for easier testing.

--- mlbgame/data.py
+++ mlbgame/data.py
@@ -8,7 +8,6 @@ gets the data from mlb.com.
 import os
 import requests
 import gzip
-import lxml
 from lxml import etree
 import datetime
 
@@ -25,7 +24,7 @@ def access_error(name):
 
 
 def unpack_ymd(date, string=False):
-    """Calculate year, month, day components from datetime.date.""" 
+    """Calculate year, month, day components from datetime.date."""
     if not string:
         return date.year, date.month, date.day
     else:
@@ -46,23 +45,17 @@ def get_data(date, file, gid=None, gz=False, no_cache=False):
     """Get xml (cached file if possible), parse w/ lxml, and return result."""
     file_path = _get_path(date, file, gid, gz)
 
-    if os.path.isfile(file_path) and not no_cache:
-        try:
-            parsed = etree.parse(file_path)
-        except lxml.etree.XMLSyntaxError:
-            print("xml syntax error: {0}".format(file_path))
-            return False
-    else:
-        url = _get_url(date, file, gid)
-        try:
-            parsed = etree.parse(url)
-            cache_xml(parsed, date, file, gid, gz)
-        except OSError:
-            print("unable to fetch: {0}".format(url))
+    if not os.path.isfile(file_path) or no_cache:
+        success = download_xml(date, file, gid, gz)
+        if not success:
             return False
 
-    # root = parsed.getroot()
-    # return root
+    if gz:
+        raw = read_xml_from_gzip(file_path)
+    else:
+        raw = read_xml_from_file(file_path)
+    parsed = etree.XML(raw.encode())
+
     return parsed
 
 
@@ -97,65 +90,106 @@ def _get_dir(date, gid=None):
     date_dir = os.path.join(ROOT_DIR, ymd)
 
     if gid:
-        return os.path.join(date_dir, 'gid_{0}'.format(gid))
+        path = os.path.join(date_dir, 'gid_{0}'.format(gid))
     else:
-        return os.path.join(date_dir)
+        path = os.path.join(date_dir)
+
+    return mkdir(path)
 
 
-def cache_xml(lxml_etree, date, file, gid=None, gz=False):
-
-    dir_path = _get_dir(date, gid)
-
+def mkdir(dir_path):
     if not os.path.exists(dir_path):
         try:
             os.makedirs(dir_path)
         except OSError:
             access_error(dir_path)
+            return False
+    return dir_path
+
+
+def download_xml(date, file, gid, gz):
+    url = _get_url(date, file, gid)
+    resp = requests.get(url)
+    raw = resp.text
+    if resp.status_code == requests.codes.ok:
+        save_xml(raw, date, file, gid, gz)
+        return True
+    else:
+        return False
+
+
+def save_xml(xml, date, file, gid=None, gz=False):
 
     file_path = _get_path(date, file, gid, gz)
-
-    xml_string = etree.tostring(lxml_etree, encoding='utf-8', standalone='yes')
-    write_to_file(xml_string, file_path, gz)
+    try:
+        if gz:
+            write_xml_to_gzip(xml, file_path)
+        else:
+            write_xml_to_file(xml, file_path)
+    except:
+        print('error writing')
     return
 
 
-def write_to_file(text, file_path, gz=False):
-    if gz:
-        with gzip.open(file_path, "w") as fi:
-            fi.write(text)
-    else:
-        with open(file_path, "w") as fi:
-            fi.write(str(text))
-    return
+def write_xml_to_file(xml, path):
+    with open(path, "w") as f:
+        f.write(xml)
+    return True
 
 
-def get_scoreboard(game_date):
+def write_xml_to_gzip(xml, path):
+    with gzip.open(path, "w") as f:
+        try:
+            f.write(xml)
+        except TypeError:
+            f.write(xml.encode())
+    return True
+
+
+def read_xml_from_file(path):
+    with open(path, "r") as f:
+        xml = f.read()
+    return xml
+
+
+def read_xml_from_gzip(path):
+    with gzip.open(path, "r") as f:
+        xml = f.read()
+    xml = xml.decode()
+    return xml
+
+
+def get_xml_from_url(url):
+    pass
+
+
+def get_scoreboard(game_date, gz=True, no_cache=False):
     """Return the game file for a certain day matching certain criteria."""
-    data = get_data(game_date, "scoreboard.xml", gz=True)
+    data = get_data(game_date, "scoreboard.xml", False, gz, no_cache)
     return data
 
 
-def get_box_score(game_id):
+def get_box_score(game_id, gz=False, no_cache=False):
     """Return the box score file of a game with matching id."""
     # get relevant information from game id
     game_date = date_from_gameid(game_id)
-    data = get_data(game_date, "boxscore.xml", game_id)
+    data = get_data(game_date, "boxscore.xml", game_id, gz, no_cache)
     return data
 
 
-def get_game_events(game_id):
+def get_game_events(game_id, gz=False, no_cache=False):
     """Return the game events file of a game with matching id."""
     # get relevant information from game id
     game_date = date_from_gameid(game_id)
-    data = get_data(game_date, "game_events.xml")
+    data = get_data(game_date, "game_events.xml", game_id, gz, no_cache)
     return data
 
 
-def get_overview(game_id):
+def get_overview(game_id, gz=False, no_cache=False):
     """Return the linescore file of a game with matching id."""
     # get relevant information from game id
     game_date = date_from_gameid(game_id)
-    data = get_data(game_date, "linescore.xml", game_id)
+    data = get_data(game_date, "linescore.xml", game_id, gz, no_cache)
     return data
 
 
