ec74533e4eb7331e9119131053382091b7647a75
Cullen Garvey <cullen.garvey@gmail.com>
2016-12-06 08:19:53 -0600

    Updated data tests based on changes to data.py.

--- mlbgame/tests/test_data.py
+++ mlbgame/tests/test_data.py
@@ -1,8 +1,12 @@
 
 import os
+import lxml
 import pytest
 import datetime
 import mlbgame.data
+
+
+date = datetime.date(2016, 11, 2)
 
 
 def test_valid_rootdir():
@@ -16,7 +20,6 @@ def test_access_error():
 
 
 def test_unpack_ymd_ints():
-    date = datetime.date(2016, 11, 2)
     y, m, d = mlbgame.data.unpack_ymd(date)
     assert y == 2016
     assert m == 11
@@ -24,7 +27,6 @@ def test_unpack_ymd_ints():
 
 
 def test_unpack_ymd_str():
-    date = datetime.date(2016, 11, 2)
     y, m, d = mlbgame.data.unpack_ymd(date, string=True)
     assert y == '2016'
     assert m == '11'
@@ -40,12 +42,39 @@ def test_date_from_gameid():
 
 
 def test__get_url():
-    date = datetime.date(2016, 11, 2)
-    _ = mlbgame.data._get_url(date, 'scoreboard.xml')
-    assert _ == 'http://gd2.mlb.com/components/game/mlb/year_2016/month_11/day_02/scoreboard.xml'
+    url = mlbgame.data._get_url(date, 'scoreboard.xml')
+    assert url == 'http://gd2.mlb.com/components/game/mlb/year_2016/month_11/day_02/scoreboard.xml'
 
 
 def test__get_path():
-    date = datetime.date(2016, 11, 2)
-    _ = mlbgame.data._get_path(date, 'scoreboard.xml')
-    assert _[-41:] == "/year_2016/month_11/day_02/scoreboard.xml"
+    path = mlbgame.data._get_path(date, 'scoreboard.xml')
+    assert path[-41:] == "/year_2016/month_11/day_02/scoreboard.xml"
+    assert os.path.exists(os.path.dirname(path))
+
+
+def test__get_dir():
+    path = mlbgame.data._get_dir(date)
+    assert path[-26:] == "/year_2016/month_11/day_02"
+    assert os.path.exists(os.path.dirname(path))
+
+
+def test_get_data_nocache():
+    data = mlbgame.data.get_data(date, 'scoreboard.xml', gz=False, no_cache=True)
+    assert isinstance(data, lxml.etree._Element)
+
+
+def test_get_data_nocache_gz():
+    data = mlbgame.data.get_data(date, 'scoreboard.xml', gz=True, no_cache=True)
+    assert isinstance(data, lxml.etree._Element)
+
+
+def test_get_data_cache():
+    data = mlbgame.data.get_data(date, 'scoreboard.xml', gz=False)
+    assert isinstance(data, lxml.etree._Element)
+
+
+def test_get_data_cache_gz():
+    data = mlbgame.data.get_data(date, 'scoreboard.xml', gz=True)
+    assert isinstance(data, lxml.etree._Element)
+
+
