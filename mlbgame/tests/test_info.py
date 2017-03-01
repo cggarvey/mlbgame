a9a01071451283b28b3f5633b18cc169b9b94d37
Cullen Garvey <cullen.garvey@gmail.com>
2017-03-01 17:10:47 -0600

    Added unit tests for the info and object modules.

--- mlbgame/tests/test_info.py
+++ mlbgame/tests/test_info.py
@@ -1,4 +1,51 @@
 import pytest
+import mlbgame
+
+
+@pytest.fixture(scope='session')
+def league_info():
+    return mlbgame.info.league_info()
+
+
+@pytest.fixture(scope='session')
+def team_info():
+    return mlbgame.info.team_info()
+
+
+@pytest.fixture(scope='session')
+def dbacks(team_info):
+    for team_dict in team_info:
+        if team_dict['club'] == 'ari':
+            info_obj = mlbgame.info.Info(team_dict)
+    return info_obj
+
+
+def test_league_info(league_info):
+
+    assert isinstance(league_info, dict)
+    assert league_info['club'] == 'mlb'
+    assert league_info['club_full_name'] == 'Major League Baseball'
+    assert league_info['display_code'] == 'mlb'
+    assert league_info['id'] == '35003'
+    assert league_info['url_prod'] == 'www.mlb.com'
+
+
+def test_team_info(team_info):
+    assert isinstance(team_info, list)
+    assert len(team_info) >= 30
+
+
+def test_info_init_setattr(dbacks):
+
+    assert dbacks.club == 'ari'
+    assert dbacks.club_common_name == 'D-backs'
+    assert dbacks.club_full_name == 'Arizona Diamondbacks'
+
+
+def test_info_nice_output(dbacks):
+    assert dbacks.nice_output() == 'Arizona Diamondbacks (ARI)'
+    assert str(dbacks) == 'Arizona Diamondbacks (ARI)'
+
 
 if __name__ == '__main__':
     pass
--- mlbgame/tests/test_object.py
+++ mlbgame/tests/test_object.py
@@ -1,4 +1,30 @@
 import pytest
+import mlbgame
+
+
+@pytest.fixture(scope='session')
+def data():
+    d = dict()
+    d['ab'] = '3'
+    d['batter_side'] = 'L'
+    d['s_avg'] = '0.350'
+    return d
+
+
+def test_init_mlbgame_object(data):
+    o = mlbgame.object.Object(data)
+
+    assert isinstance(o, mlbgame.object.Object)
+
+    assert o.ab == 3
+    assert isinstance(o.ab, int)
+
+    assert o.batter_side == 'L'
+    assert isinstance(o.batter_side, str)
+
+    assert o.s_avg == 0.350
+    assert isinstance(o.s_avg, float)
+
 
 if __name__ == '__main__':
     pass
